23c05a97fcbfa638d85ecea1e420e1e171197bc6
Abhiraj235 <abhishekrajwar8445@gmail.com>
2025-05-06 14:54:35 +0530

    file

--- DrowsinessDetector.py
+++ DrowsinessDetector.py
@@ -15,9 +15,7 @@ main.geometry("800x600")
 def EAR(drivereye):
     point1 = dist.euclidean(drivereye[1], drivereye[5])
     point2 = dist.euclidean(drivereye[2], drivereye[4])
-    # compute the euclidean distance between the horizontal
     distance = dist.euclidean(drivereye[0], drivereye[3])
-    # compute the eye aspect ratio
     ear_aspect_ratio = (point1 + point2) / (2.0 * distance)
     return ear_aspect_ratio
 
