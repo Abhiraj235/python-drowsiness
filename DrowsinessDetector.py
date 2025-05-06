20f881c58e1385e0c615effa345e44922427df8a
Abhiraj235 <abhishekrajwar8445@gmail.com>
2025-05-06 14:56:50 +0530

    commit

--- DrowsinessDetector.py
+++ DrowsinessDetector.py
@@ -20,7 +20,6 @@ def EAR(drivereye):
     return ear_aspect_ratio
 
 def MOR(drivermouth):
-    # compute the euclidean distances between the horizontal
     point   = dist.euclidean(drivermouth[0], drivermouth[6])
     # compute the euclidean distances between the vertical
     point1  = dist.euclidean(drivermouth[2], drivermouth[10])
