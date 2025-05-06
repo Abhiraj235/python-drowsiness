32ce8e1622c813be4ce60fd8e72806cfdd66cf19
Abhiraj235 <abhishekrajwar8445@gmail.com>
2025-05-06 15:02:08 +0530

    Commit

--- DrowsinessDetector.py
+++ DrowsinessDetector.py
@@ -23,7 +23,6 @@ def MOR(drivermouth):
     point   = dist.euclidean(drivermouth[0], drivermouth[6])
     point1  = dist.euclidean(drivermouth[2], drivermouth[10])
     point2  = dist.euclidean(drivermouth[4], drivermouth[8])
-    # taking average
     Ypoint   = (point1+point2)/2.0
     # compute mouth aspect ratio
     mouth_aspect_ratio = Ypoint/point
